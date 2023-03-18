d9e4b37b5ac9611e2092f945007764ff62f7cfac
Daniel Fonseca Alves <dalraf@gmail.com>
2023-03-18 17:06:41 -0300

    Melhoria na exibição

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from openfire_sync import executa_lista, retorna_lista
+import pandas as pd
 
 st.set_page_config(
     page_title="Coopemg Openfire Sync",
@@ -8,44 +9,43 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.markdown("### Atualização de usuários do Openfire")
-
-
+# Função para obter a lista de usuários a serem atualizados
 def retorna_lista_func():
     openfire, lista_alteracao = retorna_lista()
     st.session_state.lista_alteracao = lista_alteracao
     st.session_state.openfire = openfire
 
+# Função para executar a atualização
 def executa_lista_func():
     lista_alteracao = st.session_state.lista_alteracao
     openfire = st.session_state.openfire
     executa_lista(openfire, lista_alteracao)
     st.session_state.execucao = True
 
+# Título da página
+st.title("Coopemg Openfire Sync")
 
+# Botões para obter e executar a lista de atualização
+st.subheader("Atualização de usuários do Openfire")
+st.write("Clique nos botões abaixo para obter e executar a lista de atualização.")
 
-st.button("Pegar lista de atualização", on_click=retorna_lista_func)
-if 'lista_alteracao' in st.session_state:
-    st.button("Executar atualização", on_click=executa_lista_func)
+btn_retorna_lista = st.button("Obter lista de atualização", on_click=retorna_lista_func)
+btn_executa_lista = st.button("Executar atualização", on_click=executa_lista_func)
 
+# Mensagem de sucesso após a execução
 if 'execucao' in st.session_state:
-    st.markdown('---')
-    st.markdown('Alteração realizada com sucesso!')
+    st.success('Alteração realizada com sucesso!')
 
-
+# Tabela com a lista de usuários a serem atualizados
 if 'lista_alteracao' in st.session_state:
     lista_alteracao = st.session_state.lista_alteracao
-    st.markdown('---')
-    st.markdown(f'##### Numero de usuários a serem atualizados: {len(lista_alteracao)} ')
+    st.subheader(f"Lista de usuários a serem atualizados ({len(lista_alteracao)})")
+    table = []
     for line in lista_alteracao:
         usuario = line[0]
         descricao_antiga = line[1]
         descricao_nova = line[2]
-        st.markdown('---')
-        st.markdown(f'Usuário: {usuario}')
-        st.markdown(f'Descrição antiga: *{descricao_antiga}*')
-        st.markdown(f'Descrição nova: *{descricao_nova}*')
-
-
-
-    
+        table.append([usuario, descricao_antiga, descricao_nova])
+    columns = ["Usuário", "Descrição antiga", "Descrição nova"]
+    df = pd.DataFrame(table, columns=columns)
+    st.table(df)
